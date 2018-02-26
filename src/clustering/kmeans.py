8d6a5f64dbc29b495aa540d8d03a6b915d276f81
Oleg Baskov <obaskov@gmail.com>
2018-02-26 13:51:49 +0300

    POC-Turtle-4: Learning grammar from word cluster relations.

--- src/clustering/kmeans.py
+++ src/clustering/kmeans.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.metrics import pairwise_distances, silhouette_score
 
@@ -19,3 +20,36 @@ def cluster_kmeans(n_clusters, df, words_list):     # 80209 POC-Turtle-1
 
     return silhouette, inertia, labels, centroids, \
            cluster_sizes, cluster_word_ids, cluster_words
+
+
+def cluster_words_kmeans(words_df, n_clusters):     # 80219 POC-Turtle-4
+    # 80219 update cluster_kmeans 80209: dataframes, in and out
+    from sklearn.cluster import KMeans
+    from sklearn.metrics import pairwise_distances, silhouette_score
+    words_list = words_df['word'].tolist()
+    df = words_df.copy()
+    del df['word']
+    kmeans_model = KMeans(init='k-means++', n_clusters=n_clusters, n_init=10)
+    kmeans_model.fit(df)
+    labels = kmeans_model.labels_
+    inertia  = kmeans_model.inertia_
+    centroids = np.asarray(kmeans_model.cluster_centers_[:(max(labels)+1)])
+    silhouette = silhouette_score(df, labels, metric ='euclidean')
+
+    cdf = pd.DataFrame(centroids)
+    cdf = cdf.applymap(lambda x: x if abs(x) > 1e-12 else 0.)
+    cdf.columns = [x+1 if type(x)==int else x for x in cdf.columns]
+    cols = cdf.columns.tolist()
+    def cluster_word_list(i):
+        return [words_list[j] for j,x in enumerate(labels) if x==i]
+    cdf['cluster'] = cdf.index
+    cdf['cluster_words'] = cdf['cluster'].apply(cluster_word_list)
+    #+cdf = cdf.sort_values(by=[1,2,3], ascending=[True,True,True])
+    cdf = cdf.sort_values(by=[1,2], ascending=[True,True])
+    cdf.index = range(1, len(cdf)+1)
+    def cluster_id(row): return 'C' + str(row.name).zfill(2)
+    cdf['cluster'] = cdf.apply(cluster_id, axis=1)
+    cols = ['cluster', 'cluster_words'] + cols
+    cdf = cdf[cols]
+
+    return cdf, silhouette, inertia
--- src/clustering/similarity.py
+++ src/clustering/similarity.py
@@ -19,3 +19,30 @@ def cosine_similarity(centroids, cluster_words, verbose='none'):
     sorted_df = sim_df.sort_values(by=['similarity','c1','c2'], \
         ascending=[False,True,True]) # \
     return sorted_df, {'clusters_similarity_file': 'none'}
+
+
+def cluster_similarity(clusters_df, verbose='none'):    # 80219,21
+    # ++>> check sim = 1 - distance - sure?
+    from scipy import spatial
+    sim_df = pd.DataFrame(columns=['c1','c2','similarity','c1_words','c2_words'])
+    df = clusters_df.copy()
+    if 'cluster' in df.columns: del df['cluster']
+    if 'cluster_words' in df.columns: del df['cluster_words']
+    # TODO? check and delete non-number columns?
+    k = 0
+    for i in range(len(df)):
+        ccv1 = df.iloc[i].tolist()  # cluster centroid vector
+        for j in range(i+1, len(df)):
+            ccv2 = df.iloc[j].tolist()
+            sim = 1 - spatial.distance.cosine(ccv1, ccv2)   # sure?
+            if i < j:
+                c1 = clusters_df.iloc[i]['cluster']
+                c2 = clusters_df.iloc[j]['cluster']
+                c1_words = clusters_df.iloc[i]['cluster_words']
+                c2_words = clusters_df.iloc[j]['cluster_words']
+                sim_df.loc[k] = [c1, c2, sim, c1_words, c2_words]
+                k += 1
+    #-sim_df[['c1','c2']] = sim_df[['c1','c2']].astype(int)
+    return sim_df.sort_values(by=['similarity','c1','c2'], \
+                              ascending=[False,True,True]), \
+        {'clusters_similarity_file': 'none'}

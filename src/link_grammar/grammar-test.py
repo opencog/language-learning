3c1e4a60118f24aba9ed6c9584619b4c18cf0872
Alexey Glushchenko <alex-gl@mail.ru>
2018-03-26 14:45:37 +0900

    Option '-r' was added

--- src/link_grammar/grammar-test.py
+++ src/link_grammar/grammar-test.py
@@ -3,7 +3,7 @@
 import sys, re, os, shutil, locale, getopt
 from linkgrammar import LG_Error, Linkage, Sentence, ParseOptions, Dictionary, Clinkgrammar as clg
 
-__version__             = "1.3"
+__version__             = "1.4"
 DEFAULT_LG_DICT_PATH    = "/usr/local/share/link-grammar"
 OUTPUT_DIAGRAM          = 0
 OUTPUT_POSTSCRIPT       = 1
@@ -36,22 +36,118 @@ def main(argv):
     """
 
     file_path           = None
+    file_dir            = None
     lg_dict_path        = None
     template_dict_path  = None
     corpus_path         = None
     server_ip           = None
     output_id           = None
+    is_dir_specified    = None
+    is_remove_allowed   = False
+
+    def test_grammar_file(file_path, template_dict_path):
+        """
+        test_grammar_file - test each grammar file to be able to parse test file
+
+        :param file_path:
+        :return:
+        """
+
+        def get_dir_name(file_name):
+            """
+            get_dir_name - extract template grammar directory name and a name for a new grammar directory
+
+            :param file_name: grammar file name having well defined name notation
+            :return: tuple (template_grammar_directory_name, grammar_directory_name)
+            """
+            p = re.compile(
+                '(/?([._\w\d~-]*/)*)(([a-zA-Z-]+)_[0-9]{1,3}C_[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9A-F]{4})\.(4\.0\.dict)')
+            m = p.match(file_name)
+
+            retval = (None, None) if m is None else (m.group(4), m.group(3))
+            return (retval)
+
+        # Extract grammar name and a name of the new grammar directory from the file name
+        (template_dict_name, dict_path) = get_dir_name(file_path)
+
+        if dict_path is None:
+            print("Error: Unable to parse file name.")
+            exit(2)
+
+        # if '-t' is not set and '-d' is not specified
+        if template_dict_path == DEFAULT_LG_DICT_PATH:
+            template_dict_path += "/" + template_dict_name
+
+        dict_path = lg_dict_path + "/" + dict_path
+
+        print(template_dict_path, dict_path, sep="\n")
+
+        try:
+            if os.path.isdir(dict_path):
+                print("Info: Directory '" + dict_path + "' already exists.")
+
+                if is_remove_allowed:
+                    shutil.rmtree(dict_path, True)
+                    print("Info: Directory '" + dict_path + "' has been removed. Option '-r' was specified.")
+
+            if not os.path.isdir(dict_path):
+                # Create dictionary directory using existing one as a template
+                shutil.copytree(template_dict_path, dict_path)
+                print("Info: Directory '" + dict_path + "' with template files has been created.")
+
+                # Replace dictionary file '4.0.dict' with a new one
+                shutil.copy(file_path, dict_path + "/4.0.dict")
+                print("Info: Dictionary file has been replaced with '" + dict_path + "'.")
+
+            # Run Link Grammar parser
+            parse_text(dict_path, corpus_path, output_id)
+
+        except LG_Error as err:
+            print(str(err))
+
+        except IOError as err:
+            print(str(err))
+
+        except FileNotFoundError as err:
+            print(str(err))
+
+        except OSError as err:
+            print(str(err))
+
+
+    def test_grammar_dir(file_dir, template_dict_path):
+        """
+        Test multiple grammar files located in specified dictionary
+
+        :param file_dir: directory path
+        :return:
+        """
+        directory = os.fsencode(file_dir)
+
+        # Iterate through the directory
+        for file in os.listdir(directory):
+            filename = os.fsdecode(file)
+            if filename.endswith(".dict"):
+                file_name = file_dir + "/" + (str(file).replace("b'", "")).replace("'", "")
+                test_grammar_file(file_name, template_dict_path)
+
+
+    print("Grammar test script v." + __version__)
 
     try:
-        opts, args = getopt.getopt(argv, "hf:g:t:c:o:u:", ["help", "file=", "grammar-root=", "template-dir=", "test-corpus=",
+        opts, args = getopt.getopt(argv, "hrf:d:g:t:c:o:u:", ["help", "remove", "file=", "dict-file-dir=", "grammar-root=", "template-dir=", "test-corpus=",
                                                            "output-format", "server-url"])
 
         for opt, arg in opts:
             if opt in ("-h", "--help"):
-                print_help()
-                exit()
+                print(main.__doc__)
+                exit(0)
+            if opt in ("-r", "--remove"):
+                is_remove_allowed = True
             elif opt in ("-f", "--file"):
                 file_path = arg
+            elif opt in ("-d", "--dict-file-dir"):
+                file_dir = arg
             elif opt in ("-g", "--grammar-root"):
                 lg_dict_path = arg
             elif opt in ("-t", "--template-dir"):
@@ -75,95 +171,58 @@ def main(argv):
                 server_ip = arg
 
     except getopt.GetoptError:
-        print_help()
+        print(main.__doc__)
         exit(1)
 
-    if file_path is None:
-        print("Error: Dictionary file path is not specified.")
-        print_help()
+    if file_path is None and file_dir is None:
+        print("Error: Neither dictionary file path nor dictionary file directory is specified.")
+        print(main.__doc__)
         exit(1)
 
     if lg_dict_path is None:
         print("Error: Dictionary root path is not specified.")
-        print_help()
+        print(main.__doc__)
         exit(1)
 
-    if corpus_path is None:
-        print("Error: Test corpus file path is not specified.")
-        print_help()
-        exit(1)
-
-    # Extract grammar name and a name of the new grammar directory from the file name
-    (template_dict_name, dict_path) = get_dir_name(file_path)
-
-    if dict_path is None:
-        print("Error: Unable to parse file name.")
-        exit(2)
+    # '-f' option has higher priority over '-d' option, so if the first one is specified
+    #       the former is ignored
+    is_dir_specified = False if file_path is not None else True
 
     # If template dictionary path is not specified use default LG dictionary path and language name
     #   extracted from dictionary file name
     if template_dict_path is None:
-        template_dict_path = DEFAULT_LG_DICT_PATH + "/" + template_dict_name
+        template_dict_path = DEFAULT_LG_DICT_PATH
         print("Warning: Template dictionary path is not specified. Default path '{}' is used instead.".format(DEFAULT_LG_DICT_PATH))
 
-    dict_path = lg_dict_path + "/" + dict_path
+        if is_dir_specified:
+            print("Error: option '-t' should be explicitly specified when '-d' option is set.")
+            exit(3)
 
-    print(template_dict_path, dict_path, sep="\n")
+    if corpus_path is None:
+        print("Error: Test corpus file path is not specified.")
+        print(main.__doc__)
+        exit(1)
 
-    try:
-        # If the dictionary directory does not exist
-        if not os.path.isdir(dict_path):
+    # If option '-d' is specified then run through all .dict files in the dictionary
+    if is_dir_specified:
+        test_grammar_dir(file_dir, template_dict_path)
 
-            # Create dictionary directory using existing one as a template
-            shutil.copytree(template_dict_path, dict_path)
-            print("Info: Directory '" + dict_path + "' with template files has been created.")
+    # Usual run for just one .dict file
+    else:
+        test_grammar_file(file_path, template_dict_path)
 
-            # Replace dictionary file '4.0.dict' with a new one
-            shutil.copy(file_path, dict_path + "/4.0.dict")
-            print("Info: Dictionary file has been replaced with '" + dict_path + "'.")
-        else:
-            print("Info: Directory '" + dict_path + "' already exists.")
 
-        # Run Link Grammar parser
-        parse_text(dict_path, corpus_path, output_id)
-
-    except LG_Error as err:
-        print(str(err))
-        exit(2)
-
-    except IOError as err:
-        print(str(err))
-        exit(3)
-
-    except FileNotFoundError as err:
-        print(str(err))
-        exit(4)
-
-    except OSError as err:
-        print(str(err))
-        exit(5)
-
-#
-# Print usage info
-#
-def print_help():
-    print("Grammar test script v." + __version__)
-    print(main.__doc__)
-#
-# Extract the first part of the file name to use it as a name for grammar directory
-#
-def get_dir_name(file_name):
-    p = re.compile('(/?([._\w\d~-]*/)*)(([a-zA-Z-]+)_[0-9]{1,3}C_[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9A-F]{4})\.(4\.0\.dict)')
-    m = p.match(file_name)
-
-    retval = (None, None) if m is None else (m.group(4), m.group(3))
-    return (retval)
-
-#
-# Link parser invocation routine
-#
 def parse_text(dict_path, corpus_path, output_id=OUTPUT_DIAGRAM):
+    """
+    Link parser invocation routine
+
+    :param dict_path: name or path to the dictionary
+    :param corpus_path: path to the test text file
+    :param output_id: numetic type of one of three possible output format
+    :return:
+    """
     def s(q):
+        """ Helper routine """
         return '' if q == 1 else 's'
 
     def linkage_stat(psent, lang, lkgs, sent_po):

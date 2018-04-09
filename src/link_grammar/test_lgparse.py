bfa10e15705a32d527008b1b9ee04dfe81e3987f
Alexey Glushchenko <alex-gl@mail.ru>
2018-04-09 09:51:18 +0900

    Unit test file for lgparse.py added

---
+++ src/link_grammar/test_lgparse.py
@@ -0,0 +1,117 @@
+import unittest
+import sys
+import  os
+from link_grammar.lgparse import strip_token, parse_tokens, parse_links, calc_stat, parse_postscript, parse_text, \
+    create_grammar_dir, LGParseError, BIT_STRIP, BIT_RWALL, BIT_CAPS, BIT_ULL_IN, BIT_OUTPUT_DIAGRAM, \
+    BIT_OUTPUT_POSTSCRIPT, BIT_OUTPUT_CONST_TREE
+
+class TestStringMethods(unittest.TestCase):
+    postscript_str = "[(LEFT-WALL)(Dad[!])(was.v-d)(not.e)(a)(parent.n)(before)(.)(RIGHT-WALL)][[0 7 2 (Xp)][0 1 0 (Wd)][1 2 0 (Ss*s)][2 5 1 (Osm)][2 3 0 (EBm)][4 5 0 (Ds**c)][5 6 0 (Mp)][7 8 0 (RW)]][0]"
+    token_str = "(LEFT-WALL)(Dad[!])(was.v-d)(not.e)(a)(parent.n)(before)(.)(RIGHT-WALL)"
+    link_str = "[0 7 2 (Xp)][0 1 0 (Wd)][1 2 0 (Ss*s)][2 5 1 (Osm)][2 3 0 (EBm)][4 5 0 (Ds**c)][5 6 0 (Mp)][7 8 0 (RW)]"
+
+    @staticmethod
+    def cmp_lists(list1, list2) -> bool:
+        if list1 is None or list2 is None or len(list1) != len(list2):
+            return False
+
+        for i in range(0, len(list1)):
+            if list1[i] != list2[i]:
+                return False
+
+        return True
+
+    def test_strip_token(self):
+        self.assertEqual(strip_token("strange[!]"), "strange")
+        self.assertEqual(strip_token("strange.a"), "strange")
+        self.assertEqual(strip_token("[strange]"), "[strange]")
+
+    def test_parse_tokens(self):
+        options = 0
+
+        # No RIGHT-WALL, no CAPS
+        options |= BIT_STRIP
+        tokens = parse_tokens(self.token_str, options)
+        self.assertTrue(TestStringMethods.cmp_lists(tokens, ['###LEFT-WALL###', 'dad', 'was', 'not', 'a',
+                                                             'parent', 'before', '.']))
+
+        # RIGHT-WALL and CAPS, no STRIP
+        options |= (BIT_RWALL | BIT_CAPS)
+        options &= ~BIT_STRIP
+        tokens = parse_tokens(self.token_str, options)
+        self.assertTrue(TestStringMethods.cmp_lists(tokens, ['###LEFT-WALL###', 'Dad[!]', 'was.v-d', 'not.e', 'a',
+                                                             'parent.n', 'before', '.', '###RIGHT-WALL###']))
+
+    def test_parse_links(self):
+        links = parse_links(self.link_str, ['###LEFT-WALL###', 'dad', 'was', 'not', 'a', 'parent', 'before', '.'])
+
+        # [0 7 2 (Xp)][0 1 0 (Wd)][1 2 0 (Ss*s)][2 5 1 (Osm)][2 3 0 (EBm)][4 5 0 (Ds**c)][5 6 0 (Mp)][7 8 0 (RW)]
+        self.assertTrue(TestStringMethods.cmp_lists(links, [ (0, '###LEFT-WALL###', 7, '.'),
+                                                             (0, '###LEFT-WALL###', 1, 'dad'),
+                                                             (1, 'dad', 2, 'was'),
+                                                             (2, 'was', 5, 'parent'),
+                                                             (2, 'was', 3, 'not'),
+                                                             (4, 'a', 5, 'parent'),
+                                                             (5, 'parent', 6, 'before') ]))
+
+    def test_calc_stat(self):
+        f, n, s = calc_stat(['###LEFT-WALL###', 'dad', 'was', 'not', 'a', 'parent', 'before', '.'])
+        # print(f, n, s)
+        self.assertTrue(f and (not n) and (s > 0.99))
+
+        f, n, s = calc_stat(['[qqq]', '[www]', '[eee]', '[rrr]', '[ttt]', '[yyy]', '[uuu]', '[.]'])
+        # print(f, n, s)
+        self.assertTrue(n and (not f) and (s < 0.1))
+
+        f, n, s = calc_stat(['###LEFT-WALL###', 'dad', 'was', 'not', '[a]', '[parent]', '[before]'])
+        # print(f, n, s)
+        self.assertTrue((not f) and (not n) and (s - 0.5 < 0.01))
+
+    def test_parse_postscript(self):
+        options = 0
+        options |= (BIT_RWALL | BIT_CAPS)
+        options &= ~BIT_STRIP
+        f, n, s = parse_postscript(TestStringMethods.postscript_str, options, sys.stdout)
+        print("Completely: {}, Unparsed: {}, Average: {}".format(f, n, s))
+        self.assertTrue(f==1 and n==0 and s-1.0 < 0.01)
+
+    def test_parse_text(self):
+        """ Test parse with default dictionary """
+        print(__doc__)
+
+        # Testing over poc-turtle corpus... 100% success is expected.
+        options = 0 | BIT_STRIP
+        f, n, s = parse_text("../../tests/test-data/dict/poc-turtle",
+                             '../../tests/test-data/corpora/poc-turtle/poc-turtle.txt',
+                             None, 1, options)
+        # print("Completely: {0}, Unparsed: {1}, Average: {2}".format(f, n, s))
+        self.assertFalse(f+n+s < 0.001)
+        self.assertTrue(f-1.0<0.01 and n<0.01 and s-1.0 < 0.01)
+
+        # Testing over poc-turtle corpus retreaved from MST-parser links output. 100% success is expected.
+        options = 0 | BIT_STRIP | BIT_ULL_IN
+        f, n, s = parse_text("../../tests/test-data/dict/poc-turtle",
+                             '../../tests/test-data/parses/poc-turtle-mst/poc-turtle-opencog-mst-parses.txt',
+                             None, 1, options)
+        # print("Completely: {0}, Unparsed: {1}, Average: {2}".format(f, n, s))
+        self.assertFalse(f+n+s < 0.001)
+        self.assertTrue(f-1.0<0.01 and n<0.01 and s-1.0 < 0.01)
+
+        # Testing over poc-english corpus retreaved from hand coded links
+        f, n, s = parse_text("../../tests/test-data/dict/poc-turtle",
+                             '../../tests/test-data/parses/poc-english-mst/poc_english_noamb_parse_ideal.txt',
+                             None, 1, options)
+        print("Completely: {0:2.2f}, Unparsed: {1:2.2f}, Average: {2:2.2f}".format(f, n, s))
+        self.assertFalse(f+n+s < 0.001)
+        self.assertTrue(f-1.0<0.01 and n<0.01 and s-1.0 < 0.01)
+
+    def test_create_grammar_dir(self):
+        self.assertTrue("en" == create_grammar_dir("en", "", "", 0))
+
+        with self.assertRaises(LGParseError) as ctx:
+            create_grammar_dir("/home/alex/en", "", "", 0)
+        self.assertEqual("Dictionary path does not exist.", str(ctx.exception))
+
+
+if __name__ == '__main__':
+    unittest.main()

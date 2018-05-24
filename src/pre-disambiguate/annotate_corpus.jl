d3c97b1d0a33f5d1e168af3c4495c81d08ec1674
glicerico <suarezandres@gmail.com>
2018-05-24 18:41:37 +0800

    working annotate_corpus

--- src/pre-disambiguate/annotate_corpus.jl
+++ src/pre-disambiguate/annotate_corpus.jl
@@ -5,14 +5,32 @@
 
 #push!(LOAD_PATH, "./src/")
 
-function annotate_word(outfile, word, context, vm, dict)
-
+function annotate_word(outfile, separator, threshold, vm, dict, word, context)
+"""
+    writes word in the outfile.
+    If the word has more than one sense above threshold in vm model, 
+    its context is used to get the current sense, and
+    it's annotated with separator and sense number
+"""
+    output_word = word
+    id = get(dict.word2id, word, -1)
+    if id != -1
+        priors = expected_pi(vm, id)
+        if length(find(priors .> threshold)) > 1
+            probs = disambiguate(vm, dict, word, context)
+            best_sense = findmax(probs)[2]
+            output_word = word * separator * string(best_sense)
+        end
+    end
+    write(outfile, output_word * " ")
 end
 
-function annotate_file(corpus, vm, dict, separator, min_prob, win)
-    outfile = corpus * "_disamb"
+function annotate_file(corpus, outfile, vm, dict, separator, min_prob, win)
+"""
+   Goes through lines in corpus and calls annotate_word to re-write corpus 
+"""
     open(corpus, "r") do fi
-        open(outfile, "r") do fo
+        open(outfile, "w") do fo
             for line in eachline(fi)
                 split_line = split(line)
                 #println(split_line)
@@ -22,8 +40,10 @@ function annotate_file(corpus, vm, dict, separator, min_prob, win)
                     context = split_line[llim:ulim]
                     deleteat!(context, i[1] + 1 - llim)
                     #println(context)
-                    annotate_word(fo, i[2], context, vm, dict)
+                    annotate_word(fo, separator, min_prob, vm, dict, i[2], context)
                 end
+                seek(fo, position(fo)-1)
+                write(fo, "\n")
             end
         end
     end
@@ -52,13 +72,13 @@ s = ArgParseSettings()
     arg_type = AbstractString
     default = "@"
   "--min-prob"
-    help = "Minimum probability value to print a word sense"
+    help = "Min probability of second sense to consider a word ambiguous"
     arg_type = Float64
     default = 0.05
   "--window"
     help = "Size of window to look for context"
     arg_type = Int64
-    default = 2
+    default = 4
 end
 
 args = parse_args(ARGS, s)
@@ -69,7 +89,11 @@ separator = args["joiner"]
 min_prob = args["min-prob"]
 win = args["window"]
 corpus_dir = args["corpus-dir"]
+output_dir = args["output-dir"]
+if !isdir(output_dir) 
+    mkdir(output_dir) 
+end
 
-for file in readdir(args["corpus-dir"])
-    annotate_file(corpus_dir * "/" * file, vm, dict, separator, min_prob, win)
+for file in readdir(corpus_dir)
+    annotate_file(corpus_dir * "/" * file, output_dir * "/" * file * "_disamb", vm, dict, separator, min_prob, win)
 end

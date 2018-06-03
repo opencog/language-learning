369800309ecdd0f0fe06a56db72c034aaeecbb91
glicerico <suarezandres@gmail.com>
2018-06-03 23:06:07 +0800

    pre-disambiguate/train_multiple_adagram.sh

--- src/pre-disambiguate/train_multiple_adagram.sh
+++ src/pre-disambiguate/train_multiple_adagram.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-~/.julia/v0.4/AdaGram/train.sh $1 $2 adagram_$1_ --workers=4
+declare -a windows=(1 3)
+declare -a alphas=(0.1 0.3 0.5 0.7 0.9 1.1)
+workers=8
+dim=300
+epochs=1
 
-window
-dim?
-alpha!
-epochs?
+for window in "${windows[@]}"
+do
+	for alpha in "${alphas[@]}"
+	do
+		append=WO${workers}D${dim}E${epochs}A${alpha}W${window}
+		~/.julia/v0.4/AdaGram/train.sh $1 $2                           	\
+		adagram_${1}_${append} 	--workers=$workers --alpha=$alpha 		\
+		--dim=$dim --epochs=$epochs --window=$window 
+	done
+done

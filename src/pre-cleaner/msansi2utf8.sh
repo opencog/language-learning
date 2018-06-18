36fa65d9044a7d1f6d3d3dee533ae739f5224b5c
glicerico <suarezandres@gmail.com>
2018-06-18 20:58:24 +0800

    added MS-ANSI to UTF-8 converter

---
+++ src/pre-cleaner/msansi2utf8.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# ASuMa, June 2018
+# Replaces some MS-ANSI (CP1252) chars for the UTF-8 equivalent
+
+# Usage: <PATH>/msansi2utf8.sh inputDIR outputDIR
+
+for file in $PWD/$1/*
+do
+	echo "Processing file ${file}"
+
+	filename=$(basename -- "$file")
+	mkdir -p $2
+
+	sed -r "s/\x91/'/g; s/\x92/'/g; s/\x93/\"/g; s/\x94/\"/g" $file > "${2}/${filename}_utf8"
+	#tail -n +"$start_line" $file | head -n "$((start_line - end_line))" > trash
+done

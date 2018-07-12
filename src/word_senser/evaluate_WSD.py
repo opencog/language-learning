910832346532e3effb7594c7938d8538d6efaef3
glicerico <suarezandres@gmail.com>
2018-07-13 00:22:29 +0800

    added vscore to WSD

--- src/word_senser/evaluate_WSD.py
+++ src/word_senser/evaluate_WSD.py
@@ -33,25 +33,12 @@ def get_pairs(labels):
 
 def compute_fscore(true, pred):
     one_sense = False
-    # handling the case when there's only one sense for a word in "true"
-    # if len(np.unique(true)) == 1:
-    #     dumb, counts = np.unique(pred, return_counts=True)
-    #     # precision and recall are the same in this case
-    #     r = max(counts) / len(true)
-    #     p = r
-    #     one_sense = True
-    # else:
     true_pairs = get_pairs(true)
-    #print("true pairs {}".format(true_pairs))
     pred_pairs = get_pairs(pred)
-    #print("pred pairs {}".format(pred_pairs))
     int_size = len(set(true_pairs).intersection(pred_pairs))
-    # if there are not enough pairs to compare
-    # if len(pred_pairs) == 0 or len(true_pairs) == 0:
-    #     print("Returned ZERO")
-    #     return 0
     p = int_size / float(len(pred_pairs))
     r = int_size / float(len(true_pairs))
+    
     # in case both precision and recall are zero, return zero
     if (p + r) == 0:
         return 0
@@ -91,7 +78,7 @@ def compute_metrics(answers, predictions):
         fscore, vscore, ari (adjusted random index)
     """
     aris = []
-    #vscores = []
+    vscores = []
     fscores = []
     #weights = []
     one_sense_count = 0
@@ -102,24 +89,24 @@ def compute_metrics(answers, predictions):
         #weights.append(pred.shape[0])
         #if len(np.unique(true)) > 1:
         aris.append(adjusted_rand_score(true, pred))
-        #vscores.append(v_measure_score(true, pred))
+        vscores.append(v_measure_score(true, pred))
         fscore = compute_fscore(true, pred)
         fscores.append(fscore)
         # if one_sense == True:
         #    one_sense_count += 1
-#        print('%s: ari=%f, vscore=%f, fscore=%f' % (k, aris[-1], vscores[-1], fscores[-1]))
+        print('%s: ari=%f, vscore=%f, fscore=%f' % (k, aris[-1], vscores[-1], fscores[-1]))
     aris = np.array(aris)
-    #vscores = np.array(vscores)
+    vscores = np.array(vscores)
     fscores = np.array(fscores)
     #weights = np.array(weights)
-    print('number of one-sense words in reference: %d' % one_sense_count)
+    #print('number of one-sense words in reference: %d' % one_sense_count)
     print('mean ari: %f' % np.mean(aris))
-    #print('mean vscore: %f' % np.mean(vscores))
+    print('mean vscore: %f' % np.mean(vscores))
     #print('weighted vscore: %f' % np.sum(vscores * (weights / float(np.sum(weights)))))
     print('mean fscore: %f' % np.mean(fscores))
     #print('weighted fscore: %f' % np.sum(fscores * (weights / float(np.sum(weights)))))
-    #return np.mean(aris),np.mean(vscores),np.mean(fscores)
-    return np.mean(fscores)
+    return np.mean(aris),np.mean(vscores),np.mean(fscores)
+    #return np.mean(fscores)
 
 def main(argv):
     """
@@ -148,8 +135,8 @@ def main(argv):
         elif opt in ("-s", "--separator"):
             separator = arg
 
-    #ari_list = []
-    #vscore_list = []
+    ari_list = []
+    vscore_list = []
     fscore_list = []
     eval_files = []
     true_answers = read_answers(ref_file, separator)
@@ -157,19 +144,19 @@ def main(argv):
         print("Evaluating: {}".format(test_file))
         eval_files.append(test_file)
         predictions = read_answers(test_dir + "/" + test_file, separator)
-        #ari, vscore, fscore = compute_metrics(true_answers, predictions)
+        ari, vscore, fscore = compute_metrics(true_answers, predictions)
         fscore = compute_metrics(true_answers, predictions)
-        #ari_list.append(ari)
-        #vscore_list.append(vscore)
+        ari_list.append(ari)
+        vscore_list.append(vscore)
         fscore_list.append(fscore)
         print('\n')
 
-    #max_ari = max(ari_list)
-    #ari_indexes = [i for i, j in enumerate(ari_list) if j == max_ari]
-    #print("Best ari: {} in files {}\n".format(max_ari, [eval_files[i] for i in ari_indexes]))
-    #max_vscore = max(vscore_list)
-    #vscore_indexes = [i for i, j in enumerate(vscore_list) if j == max_vscore]
-    #print("Best vscore: {} in files {}\n".format(max_vscore, [eval_files[i] for i in vscore_indexes]))
+    max_ari = max(ari_list)
+    ari_indexes = [i for i, j in enumerate(ari_list) if j == max_ari]
+    print("Best ari: {} in files {}\n".format(max_ari, [eval_files[i] for i in ari_indexes]))
+    max_vscore = max(vscore_list)
+    vscore_indexes = [i for i, j in enumerate(vscore_list) if j == max_vscore]
+    print("Best vscore: {} in files {}\n".format(max_vscore, [eval_files[i] for i in vscore_indexes]))
     max_fscore = max(fscore_list)
     fscore_indexes = [i for i, j in enumerate(fscore_list) if j == max_fscore]
     print("Best fscore: {} in files {}\n".format(max_fscore, [eval_files[i] for i in fscore_indexes]))

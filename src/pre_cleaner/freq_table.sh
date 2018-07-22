4958926cb63a58dc007e7b702b1cac7d01dc7206
glicerico <suarezandres@gmail.com>
2018-07-22 16:58:08 +0800

    created parse_counter script

--- src/pre_cleaner/freq_table.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# creates frequency table for words in file
-# takes two arguments: corpus and otuput file name
-
-# usage: ./freq_table.sh corpus output
-
-tr " " "\n" < $1 | tr "." "\n" | tr "," "\n" | tr ";" "\n" | tr "!" "\n" | tr "?" "\n" | tr ":" "\n" | sort | uniq -c | awk '{print $2"\t"$1}' | sort -k 2,2 -r -n > $2

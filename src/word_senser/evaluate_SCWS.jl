5e5ad3b6f0f4be7f0c7e816665dd3b96e5094a24
glicerico <suarezandres@gmail.com>
2018-09-16 18:59:34 +0800

    first draft of evaluate_SCWS.jl

--- src/word_senser/evaluate_SCWS.jl
+++ src/word_senser/evaluate_SCWS.jl
@@ -9,28 +9,63 @@
 
 #push!(LOAD_PATH, "./src/")
 
-function disambiguate_word(vm, dict, word, context, window)
+function disambiguate_word(vm, dict, word, valid_context, window)
 """
-    Disambiguates a valid word using context of size window.
-    If some words in context don't exist in the model, they get replaced
-    if possible
+    Disambiguates a valid word using valid context
 """
         return disambiguate(vm, dict, word, valid_context)
 end
 
-function process_entry()
-    # id = get(dict.word2id, word, -1)
-    # if id != -1
-    #     priors = expected_pi(vm, id)
-    #     best_sense = findmax(probs)[2]
-    return true
+function similarity(vm, dict, w1, s1, w2, s2)
+"""
+    Calculates cosine distance between two sense vectors
+"""
+    v1 = dict.word2id[w1]
+    v2 = dict.word2id[w2]
+    sense_vec1 = vec(vm, v1, s1)
+    sense_vec2 = vec(vm, v2, s2)
+    return dot(sense_vec1, sense_vec2)
 end
 
-function words_in_model(vm, dict, word_pair)
+function evaluate_entry(vm, dict, word1, word2, context1, context2, min_prob=1e-2)
+"""
+    Evaluates 4 similarity measures for a pair or words
+"""
+    priors1 = expected_pi(vm, dict.word2id[word1])
+    priors2 = expected_pi(vm, dict.word2id[word2])
+    posteriors1 = disambiguate(vm, dict, word1, context1)
+    posteriors2 = disambiguate(vm, dict, word2, context2)
+
+    # AvgSim & MaxSim (no context involved)
+    # AvgSimC & MaxSimC (context taken into account)
+    similarities = []
+    for s1 in 1:T
+        if priors1[s1] > min_prob
+            for s2 in 1:T
+                if priors2[s2] > min_prob
+                    sim = similarity(vm, dict, word1, s1, word2, s2)
+                    push!(similarities, sim)
+                    push!(similaritiesC, sim * posteriors1[s1] * posteriors2[s2])
+                end
+            end
+        end
+    end
+    AvgSim = mean(similarities)
+    MaxSim = maximum(similarities)
+    AvgSimC = sum(similaritiesC)
+
+    best_sense1 = findmax(posteriors1)[2]
+    best_sense2 = findmax(posteriors2)[2]
+    MaxSimC = similarity(vm, dict, word1, best_sense1, word2, best_sense2)
+
+    return AvgSim, MaxSim, AvgSimC, MaxSimC
+end
+
+function words_in_model(dict, word_pair)
 """
     Checks if both words in pair are included in vector model
 """
-    if (word_pair[2] in dict.id2word) && (word_pair[4] in dict.id2word)
+    if (word1 in dict.id2word) && (word2 in dict.id2word)
         return true
     else
         return false
@@ -49,9 +84,34 @@ function load_SCWS(path)
     return database
 end
 
+function build_context(dict, tokens, window)
+"""
+    Builds a valid context array
+"""
+    context = tokens
+    filter!(x -> x in dict.id2word, context)
+    if length(context) > window
+        context = context[1:window]
+    end
+    return context
+end
+
+
+function filter_context(dict, text, window, word)
+"""
+    Prepares context of size 2 * window with only valid words
+"""
+    split_text = split(text, "<b> " * word * " </b>") # remove current word
+    left_text = reverse(split(split_text[1]))
+    right_text = split(split_text[2])
+    left_context = build_context(dict, left_text, window)
+    right_context = build_context(dict, right_text, window)
+    return vcat(left_context, right_context)
+end
+
 function evaluate_model(local_model, window, SCWS_PATH)
 """
-    Evaluates the given model using a context of window size when needed
+    Evaluates the given model using a context of size window
 """
 
     data = load_SCWS(SCWS_PATH)
@@ -60,15 +120,39 @@ function evaluate_model(local_model, window, SCWS_PATH)
     processed_pairs = 0
     for i = 1:size(data)[1]
         entry = data[i,:]
-        if !words_in_model(vm, dict, entry)
+        word1 = lowercase(entry[2])
+        word2 = lowercase(entry[4])
+        text1 = lowercase(entry[6])
+        text2 = lowercase(entry[7])
+        if !words_in_model(dict, word1, word2)
+            println("At least one word not in model for entry: ", i)
             continue
         end
 
-        #process_entry(vm, dict, word_pair)
+        context1 = filter_context(dict, text1, window, word1)
+        context2 = filter_context(dict, text2, window, word2)
+        if length(context1) == 0 || length(context2) == 0
+            println("Context invalid for entry id: ", i)
+            continue
+        end
+
+        scores = evaluate_entry(vm, dict, word1, word2, context1, context2)
+        Avg_count += scores[1]
+        Max_count += scores[2]
+        AvgC_count += scores[3]
+        MaxC_count += scores[4]
+
         processed_pairs += 1
     end
-    println("Processed pairs: ", processed_pairs)
 
+    println("--------------------------------------------")
+    println("Processed pairs: ", processed_pairs)
+    println("--------------------------------------------")
+    println("AvgSim: ", Avg_count / processed_pairs)
+    println("MaxSim: ", Max_count / processed_pairs)
+    println("AvgSimC: ", AvgC_count / processed_pairs)
+    println("MaxSimC: ", MaxC_count / processed_pairs)
+    println("--------------------------------------------")
 end
 
 

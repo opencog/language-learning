4d102777554c3edee148d239f6d97f806e992d3b
Oleg Baskov <obaskov@gmail.com>
2018-12-17 08:58:36 +0300

    Update ppln.py to merge to master and PR to singnet

--- pipeline/ppln.py
+++ pipeline/ppln.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# language-learning/pipeline/ppln.py :: CLI Grammar Learner + Tester    # 81217
 
 import sys, getopt, os, platform, json
 from shutil import copy2 as copy
@@ -8,28 +9,25 @@ from src.grammar_learner.utl import UTC, test_stats
 from src.grammar_learner.read_files import check_dir, check_corpus
 from src.grammar_learner.pqa_table import wide_rows
 
-__version__ = "0.0.0"
+__version__ = '0.0.1'
 
 def main(argv):
-    """ Usage: ppln <json-config-file> """
-    print('\nGrammar Learner v.' + __version__, 'started', UTC(), '\n')
-    # print("Python v." + platform.python_version())
+    """ Usage: python ppln.py config.json """
+    print('\nGrammar Learner + Tester ppln v.' + __version__, 'started', UTC(),
+          '| Python v.' + platform.python_version(), '\n')
     try:
         opts, args = getopt.getopt(argv, "h", ["help"])
-        #-print(f'opts: {opts}, args: {args}')
     except getopt.GetoptError:
         print('''Usage: ppln <json-config-file>''')
-        sys.exit(2)
+        sys.exit()
     for opt in opts:
         if opt == '-h':
             print('''Usage: ppln <json-config-file>''')
             sys.exit()
-    else:
-        config_json = args[0]
+    else: config_json = args[0]
 
     with open(config_json) as f:
         kwargs = json.load(f)
-        #-print(kwargs)
 
     corpus = kwargs['corpus']; del kwargs['corpus']
     dataset = kwargs['dataset']; del kwargs['dataset']
@@ -41,8 +39,7 @@ def main(argv):
     rp = module_path + kwargs['reference']
     if 'test_corpus' in kwargs:
         cp = module_path + kwargs['test_corpus']
-    else:
-        cp = rp  # test corpus path = reference parses path
+    else: cp = rp  # test corpus path = reference parses path
     if 'tmpath' not in kwargs:
         kwargs['tmp_dir'] = ''
     else:
@@ -51,23 +48,16 @@ def main(argv):
         else:
             if 'home' in kwargs['tmpath']:
                 tmpath = kwargs['tmpath']
-            else:
-                tmpath = module_path + kwargs['tmpath']
+            else: tmpath = module_path + kwargs['tmpath']
             if check_dir(tmpath, True, 'none'):
                 kwargs['tmp_dir'] = tmpath
-            else:
-                kwargs['tmp_dir'] = ''
-
-    # TODO: cluster_range, min_word_count ⇒ arrays ⇒ loops
+            else: kwargs['tmp_dir'] = ''
 
     a, _, hdr, log, rules = wide_rows(line, out_path, cp, rp, (1, 1), **kwargs)
 
     copy(config_json, log['project_directory'])
 
     print('\nGrammar learning and the learned grammar test ended', UTC())
-    #      '\nClean dictionary size:', log['cleaned_words'],
-    #      '\nGrammar Learner time:', log['grammar_learn_time'],
-    #      '\nGrammar test time:', log['grammar_test_time'])
     print(test_stats(log))
     print('Output directory:', log['project_directory'], '\n')
 

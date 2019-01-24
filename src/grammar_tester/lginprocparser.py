d1616a98a039417e68255d7f3998a2d699e2b2ba
Alexey Glushchenko <alex-gl@mail.ru>
2019-01-24 14:19:57 +0900

    Global variable CONFIG were added.

--- src/grammar_tester/lginprocparser.py
+++ src/grammar_tester/lginprocparser.py
@@ -289,11 +289,11 @@ class LGInprocParser(AbstractFileParserClient):
                                        "and error message:\n'{2}'.".format(lgp_cmd[0], proc_pars.returncode,
                                                                            err.decode()))
 
-                # with open(output_path + ".raw", "w") as r:
-                #     r.write(raw.decode("utf-8-sig"))
-                #
-                # with open(output_path + ".err", "w") as e:
-                #     e.write(err.decode("utf-8-sig"))
+                with open(output_path + ".raw", "w") as r:
+                    r.write(raw.decode("utf-8-sig"))
+
+                with open(output_path + ".err", "w") as e:
+                    e.write(err.decode("utf-8-sig"))
 
                 # Take an action depending on the output format specified by 'options'
                 ret_metrics, ret_quality = self._handle_stream_output(raw.decode("utf-8-sig"), options,

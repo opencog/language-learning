57e6c28acb489d5176d03afbf0b3de9203a62612
Oleg Baskov <obaskov@gmail.com>
2019-01-30 13:40:57 +0300

    CLI category_tagger utility prototype 0.0.1
    "Create category-tagger utility to replace words in any texts, such as corpora or parse files, with tags corresponding to the category labels found in the LG-file, with two inputs (text file and dictionary file), one output (tagged file) and two configurable options (suffix and prefix to encode category labels, both default to '###')" -- [ULL Project Plan](https://docs.google.com/spreadsheets/d/1TPbtGrqZ7saUHhOIi5yYmQ9c-cvVlAGqY14ATMPVCq4/edit#gid=624274537&range=C153)

--- pipeline/README.md
+++ pipeline/README.md
@@ -22,10 +22,54 @@ Fast test with [CDS-LG-E-Clean-11-50.json](https://github.com/singnet/language-l
 ## Grammar Tester CLI tests:
 
 ```
-$ ull-cli -C Check-GCB-2018-12-14-500c-mwc\=1+.json --verbosity=warning --logging=debug
+(ull) $ ull-cli -C Check-GCB-2018-12-14-500c-mwc\=1+.json --verbosity=warning --logging=debug
 ```
 
 ```
-$ python tstr.py GT-CDS-LG-E-551-cDRKc-90119.json
-$ python tstr.py GT-GCB-2018-12-14-500c-mwc=1.json
+(ull) $ python tstr.py GT-CDS-LG-E-551-cDRKc-90119.json
+(ull) $ python tstr.py GT-GCB-2018-12-14-500c-mwc=1.json
 ```
+
+## Category tagger: alpha v.0.0.1 2019-01-29 
+Replace words in .ull files with relevant categories from Link Grammar .dict. 
+
+### Usage:
+```
+    category_tagger -d <dict_path> -i <input_path> -o <output_path>  [OPTIONS]
+    category_tagger -C <json-config-file>
+
+    dict_path           Path to grammar file in Link Grammar .dict format.
+    input_path          Input corpus file or directory path.
+    output_path         Output directory path to store tagged .dict files.
+    json-config-file    JSON configuration file path.
+
+    OPTIONS:
+        -h --help       Print usage info.
+        -C <json-config-file>  All parameters set exclusively in the .json file
+                        Other parameters and options ignored.
+    """
+```
+Input files are read from the `input_path`, 
+tagged copies are saved in the `output_path/tagged_ull` directory 
+with the original names.
+
+### Making script executable:
+```
+$ cd ~/language-learning/pipeline
+$ chmod +x category_tagger 
+$ export PATH=home/your_path/language-learning/pipeline:$PATH
+```
+Otherwise run script with `$ python category_tagger <...>` command
+
+### Samples:
+```
+$ cd ~/language-learning/pipeline
+$ conda activate ull
+(ull) $ category_tagger
+(ull) $ category_tagger -h
+(ull) $ category_tagger -C CT-CDS-LG-E-551.json
+(ull) $ category_tagger -d '/output/POC-English-2019-01-25/POC-English-Amb_LG-E-551_dDRKd_gen-rules/dict_4C_2019-01-25_0006.4.0.dict'  -i '/data/POC-English-Amb/LG-E-551' -o '/output/_cat_tagger_POC-E-551'
+```
+Running with empty parameters `$ category_tagger` performs fast self-test 
+with parameters stored in the `pipeline/CT-CDS-LG-E-clean.json` configuration file.
+  
--- src/grammar_learner/read_files.py
+++ src/grammar_learner/read_files.py
@@ -1,9 +1,9 @@
-# language-learning/src/grammar_learner/read_files.py                   # 80331
+# language-learning/src/grammar_learner/read_files.py                   # 90129
 import logging
 import os
 
 
-def check_dir(dir_path, create = False, verbose='none'):
+def check_dir(dir_path, create = False, verbose = 'none'):
     logger = logging.getLogger(__name__ + ".check_dir")
     if os.path.exists(dir_path):
         return True
@@ -16,61 +16,123 @@ def check_dir(dir_path, create = False, verbose='none'):
             return False
 
 
-def check_dir_files(dir_path, verbose='none'):
+def check_dir_files(dir_path, verbose = 'none'):
     logger = logging.getLogger(__name__ + ".check_dir_files")
     files = []
     if dir_path[-1] != '/':
         path = dir_path + '/'
-    else: path = dir_path
+    else:
+        path = dir_path
     if os.path.exists(dir_path):
         logger.info(f'Directory {path} exists.')
         for filename in os.listdir(dir_path):
-            files.append(path+filename)
+            files.append(path + filename)
             logger.info(filename)
     else:
         logger.critical(f'No directory {dir_path}')
     return files
 
 
-def check_corpus(input_file, verbose='none'):
-    logger = logging.getLogger(__name__ + ".check_corpus")
-
-    if os.path.isfile(input_file):
-        logger.info(f'File exists: {input_file}')
-        logger.debug('Input corpus:\n')
-        with open(input_file, 'r') as f:
-            lines = f.read().splitlines()
-        for line in lines:
-            logger.debug(line)
-        return True
-    else:
-        if verbose != 'none':
-            logger.info(f'No corpus file: {input_file}')
-        return False
-
-
-def check_mst_files(input_dir, verbose='none'):
+def check_mst_files(input_dir, verbose = 'none'):
     logger = logging.getLogger(__name__ + ".check_mst_files")
-    if check_dir(input_dir, create=False, verbose=verbose):
-        files = check_dir_files(input_dir, verbose=verbose)
+    if check_dir(input_dir, create = False, verbose = verbose):
+        files = check_dir_files(input_dir, verbose = verbose)
         if len(files) > 0:
             logger.info(files)
             response = {'input files': files}
             for i, file in enumerate(files):
-                if check_corpus(file, verbose=verbose):
+                if os.path.isfile(file):
                     logger.info('File #' + str(i) + f' {file} checked')
                 else:
                     logger.critical('File #' + str(i) + f' {file} check failed')
             return files, response
         else:
             logger.critical(f'Input directory {input_dir} is empty')
-            return [], {'error': 'empty input directory'}
+            return [], {'check_mst_file_error': 'empty input directory'}
     else:
         logger.critical(f'No input directory {input_dir}')
-        return [], {'error': 'no input directory'}
+        return [], {'check_mst_file_error': 'no input directory'}
 
+
+def check_dict(file_path):          # TODO: update this stub            # 90128
+    if os.path.isfile(file_path):
+        return True
+    else: return False
+
+
+def check_ull(file_path):           # TODO: update this stub            # 90128
+    if os.path.isfile(file_path):
+        return True
+    else: return False
+
+
+def check_corpus(input_dir, verbose = 'none'):  # TODO: add file tests  # 90129
+    if check_dir(input_dir, False, verbose):
+        files = check_dir_files(input_dir, verbose)
+        if len(files) > 0:
+            response = {'input files': files}
+            parses = []
+            for i, file in enumerate(files):
+                if check_ull(file):
+                    with open(file, 'r') as f:
+                        lines = f.read().splitlines()
+                    if len(lines) > 0:
+                        parses.extend(lines)
+                        parses.extend([])  # empty line
+            if len(parses) > 0:
+                response.update
+                return True
+            else: return False
+        else: return False
+    else: return False
+
+
+def check_path(par, t = 'else', **kwargs):  # TODO: update stubs...     # 90129
+    # default: t = 'else': kwargs[par] is file or dir ⇒ return path
+    if 'module_path' in kwargs:
+        module_path = kwargs['module_path']
+    else:
+        return None
+    if par in kwargs:
+        path = kwargs[par]
+        if len(path) == 0:
+            path = module_path
+        elif 'home' not in path:
+            if path[0] != '/':
+                path = '/' + path
+            path = module_path + path
+    else:
+        print('"' + par + '" not in kwargs:', kwargs)
+        return None
+    if 'dir' in t:
+        if check_dir(path, True, 'max'):
+            return path
+        else: return None
+    elif 'fil' in t:  # file
+        if os.path.isfile(path):
+            return path
+        else: return None
+    elif 'dic' in t:  # 'dict', '.dict'
+        if check_dict(path):
+            return path
+        else: return None
+    elif 'cor' in t:  # corpus: dir with file(s)
+        # if check_mst_files(path, 'max'): FIXME: returns False ?
+        if check_corpus(path):                                          # 90129
+            return path
+        else: return None
+    elif 'ull' in t:  # single corpus file
+        if check_ull(path):
+            return path
+        else: return None
+    else:
+        if check_dir(path, False, 'none') or os.path.isfile(path):
+            return path
+        else: return None
 
 # Notes
 
 # 81219 @alex: logger
 # 81231 cleanup
+# 90128 check_path, stubs: check_dict, check_ull
+# TODO: cleanup, check_ull, check corpus dir or/and single file

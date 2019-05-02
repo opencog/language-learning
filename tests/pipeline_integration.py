a6e99367ef066304845bbcf749ba70262cf4b202
Alexey Glushchenko <alex-gl@mail.ru>
2019-05-02 10:37:42 +0900

    Pipeline integration test was added.

---
+++ tests/pipeline_integration.py
@@ -0,0 +1,145 @@
+import os
+import shutil
+import unittest
+from subprocess import PIPE, Popen
+
+# Root path where all LG dictionary subdirectories are located
+DICT_REL_PATH = "tests/test-data/dict"
+
+# Root path for all pipeline integration tests
+TESTS_ROOT = "tests/test-data/pipeline"
+
+
+class PipelineIntegrationTestCase(unittest.TestCase):
+    """
+    Pipeline integration test base class
+
+    """
+    @staticmethod
+    def run_pipeline(config_path: str) -> int:
+        """
+        Run pipeline defined by JSON configuration file
+
+        :param config_path:     JSON configuration file.
+        :return:                OS error level
+        """
+        ull_cli = shutil.which("ull-cli")
+
+        if ull_cli is None:
+            raise FileNotFoundError("'ull-cli' is not found.")
+
+        with Popen([ull_cli, "-C", config_path, "--logging=debug"], stdout=PIPE) as proc:
+            proc.communicate()
+
+            ret_code = proc.returncode
+
+        return ret_code
+
+    @staticmethod
+    def cmp_summaries(test_path: str, ref_path: str) -> bool:
+        """
+        Compare pipeline dash board summaries
+
+        :param test_path:       Path to summary file, produced during pipeline test run.
+        :param ref_path:        Path to reference summary file
+        :return:                True if summaries are identical, False otherwise.
+        """
+        with open(test_path, "r") as test_file:
+            test_lines = test_file.readlines()
+
+        with open(ref_path, "r") as ref_file:
+            ref_lines = ref_file.readlines()
+
+        if len(test_lines) != len(ref_lines):
+            return False
+
+        for t, r in zip(test_lines, ref_lines):
+            t_tokens = t.split()
+            r_tokens = r.split()
+
+            if len(t_tokens) != len(r_tokens):
+                return False
+
+            for tt, rr in zip(t_tokens, r_tokens):
+                if tt.strip() != rr.strip():
+                    return False
+
+        return True
+
+    @staticmethod
+    def prepare_test_dir(test_path: str, corpus_path: str) -> str:
+        """
+        Prepare pipeline test temporary directory:
+            - create temporary directory;
+            - copy all files from 'test_path' to the temporary directory;
+            - create symbol link to a corpus file/directory
+
+        :param test_path:       Path to a directory where test files reside.
+        :param corpus_path:     Path to a corpus file or directory.
+        :return:                Path to prepared test directory
+        """
+        if not os.path.isdir(test_path):
+            raise FileNotFoundError(f"'{test_path}' does not exist.")
+
+        if test_path.endswith("/"):
+            test_path = test_path[:-1]
+
+        last_slash_pos = test_path.rfind("/")
+        test_dir_name = test_path if last_slash_pos < 0 else test_path[last_slash_pos+1:]
+        path_to_create = "/var/tmp/" + test_dir_name
+
+        # Remove temporary directory if it's already there
+        if os.path.isdir(path_to_create):
+            shutil.rmtree(path_to_create)
+
+        # Copy directory pointed by 'test_path' to a new temporary directory
+        shutil.copytree(test_path, path_to_create)
+
+        pwd = os.environ["PWD"]
+
+        if os.path.isfile(corpus_path):
+            os.mkdir("data")
+            os.symlink(f"{pwd}/{corpus_path}", path_to_create + f"/data/{os.path.split(corpus_path)[1]}")
+
+        elif os.path.isdir(corpus_path):
+            os.symlink(f"{pwd}/{corpus_path}", path_to_create + f"/data", True)
+
+        else:
+            raise FileNotFoundError("Corpus file/directory does not exist.")
+
+        # Create 'dict' pseudo directory
+        os.symlink(f"{pwd}/{DICT_REL_PATH}", path_to_create + f"/dict", True)
+
+        return path_to_create
+
+    def run_pipeline_test_case(self, test_name: str, corpus_path: str) -> None:
+        """
+        Execute pipeline integration test
+
+        :param test_name:       Test test/subdirectory name.
+        :param corpus_path:     Path to a corpus file/directory.
+        :return:                None
+        """
+        test_path = f"{TESTS_ROOT}/{test_name}"
+
+        tmp_test_path = self.prepare_test_dir(test_path, corpus_path)
+
+        self.assertEqual(f"/var/tmp/{test_name}", tmp_test_path)
+        self.assertTrue(os.path.isdir(tmp_test_path))
+
+        # Run pipeline
+        self.assertEqual(0, self.run_pipeline(f"{tmp_test_path}/{test_name}.json"),
+                         f"Error executing pipeline. See '{tmp_test_path}/{test_name}.log' for details.")
+
+        summary_path = f"{tmp_test_path}/{test_name}-summary.txt"
+        reference_path = f"{tmp_test_path}/{test_name}-expected.txt"
+
+        # Check if there is a summary file in the directory
+        self.assertTrue(os.path.isfile(summary_path))
+
+        # Compare summaries
+        self.assertTrue(self.cmp_summaries(reference_path, summary_path),
+                        f"'{summary_path}' and '{reference_path}' mismatch.")
+
+        # Remove temporary directory on success
+        shutil.rmtree(tmp_test_path)
---
+++ tests/test_pipeline.py
@@ -0,0 +1,40 @@
+import unittest
+from .pipeline_integration import PipelineIntegrationTestCase
+
+"""
+ULL PIPELINE INTEGRATION TESTS
+
+In order to create additional test one should complete the following steps:
+
+1. Create proper JSON configuration script having: 
+    - corpus related parameters refer to %ROOT/data;
+    - Grammar Tester/Text Parser dictionary related variables refer to %ROOT/dict;
+    - Dash board setup to have tested values in summary table saved as %CONFIG-summary.txt.
+2. Run pipeline manually with previously created configuration file to make sure it works properly and have tested 
+    values in dash board summary text file;
+3. Rename dash board summary text file as follows: <test-name>-expected.txt to be used as reference when testing;
+3. Make subdirectory in pipeline integration test directory (tests/test-data/pipeline) having the followind structure:
+    <test-name>
+        <test-name>.json
+        <test-name>-expected.txt
+4. Create one more class method down in this file as follows:
+    def test_<test-name>(self):
+        self.run_pipeline_test_case("<test-name>", "<corpus-path>")
+        
+All <test-name> should be replaced with your test name. <corpus-path> should be replaced with path to either corpus file
+or corpus directory relative to 'language-learning' project directory. 
+Use already existing tests in (tests/test-data/pipeline) as examples when creating JSON configuration file and test
+subdirectory structure.         
+
+"""
+
+
+class PipelineTestCase(PipelineIntegrationTestCase):
+
+    def test_gl_gt(self):
+        """ GL+GT ALE50 test case performed on CDS-clean corpus """
+        self.run_pipeline_test_case("GL-GT-ALE", "data/CDS/LG-E-clean")
+
+
+if __name__ == '__main__':
+    unittest.main()

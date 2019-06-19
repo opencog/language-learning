310ad85e8f5ee574a4fbe5a8100d7b740563bb24
Alexey Glushchenko <alex-gl@mail.ru>
2019-06-19 15:07:49 +0900

    Common sed regex were fixed to handle CR/LF.

--- src/common/sedcommands.py
+++ src/common/sedcommands.py
@@ -12,13 +12,30 @@ def get_sed_regex(options: int) -> str:
     """
     # If BIT_ULL_IN sed filters links leaving only sentences and removes square brackets around tokens if any.
     if options & BIT_ULL_IN:
-        return r'/((^$)|(^([0-9]+[[:space:]]+.+){2}([[:space:]]+[-+0-9.e]+)?$))/d;s/\[([a-z0-9A-Z.,:\@"?!*~()\/\#\$&;^%_`\0xe2\x27\xE2\x80\x94©®°•…≤±×΅⁻¹²³€αβπγδμεθ«»=+-]*)\]/\1/g;s/.*/\L\0/g' \
+        return r'/((^$)|(^\x0d$)|(^([0-9]+[[:space:]]+.+){2}([[:space:]]+[-+0-9.e]+)?$))/d;s/\[([a-z0-9A-Z.,:\@"?!*~()\/\#\$&;^%_`\0xe2\x27\xE2\x80\x94©®°•…≤±×΅⁻¹²³€αβπγδμεθ«»=+-]*)\]/\1/g;s/.*/\L\0/g' \
             if options & BIT_INPUT_TO_LCASE \
-            else r'/((^$)|(^([0-9]+[[:space:]]+.+){2}([[:space:]]+[-+0-9.e]+)?$))/d;s/\[([a-z0-9A-Z.,:\@"?!*~()\/\#\$&;^%_`\0xe2\x27\xE2\x80\x94©®°•…≤±×΅⁻¹²³€αβπγδμεθ«»=+-]*)\]/\1/g'
+            else r'/((^$)|(^\x0d$)|(^([0-9]+[[:space:]]+.+){2}([[:space:]]+[-+0-9.e]+)?$))/d;s/\[([a-z0-9A-Z.,:\@"?!*~()\/\#\$&;^%_`\0xe2\x27\xE2\x80\x94©®°•…≤±×΅⁻¹²³€αβπγδμεθ«»=+-]*)\]/\1/g'
 
     # Otherwise sed removes only empty lines.
     else:
-        return r'/^$/d;s/.*/\L\0/g' if options & BIT_INPUT_TO_LCASE else r'/^$/d'
+        return r'/(^$)|(^\x0d$)/d;s/.*/\L\0/g' if options & BIT_INPUT_TO_LCASE else r'/(^$)|(^\x0d$)/d'
+
+# def get_sed_regex(options: int) -> str:
+#     """
+#     Return regular expression built according to the set of specified options.
+#
+#     :param options:     Grammar tester options.
+#     :return:            Regular expression string.
+#     """
+#     # If BIT_ULL_IN sed filters links leaving only sentences and removes square brackets around tokens if any.
+#     if options & BIT_ULL_IN:
+#         return r'/((^$)|(^([0-9]+[[:space:]]+.+){2}([[:space:]]+[-+0-9.e]+)?$))/d;s/\[([a-z0-9A-Z.,:\@"?!*~()\/\#\$&;^%_`\0xe2\x27\xE2\x80\x94©®°•…≤±×΅⁻¹²³€αβπγδμεθ«»=+-]*)\]/\1/g;s/.*/\L\0/g' \
+#             if options & BIT_INPUT_TO_LCASE \
+#             else r'/((^$)|(^([0-9]+[[:space:]]+.+){2}([[:space:]]+[-+0-9.e]+)?$))/d;s/\[([a-z0-9A-Z.,:\@"?!*~()\/\#\$&;^%_`\0xe2\x27\xE2\x80\x94©®°•…≤±×΅⁻¹²³€αβπγδμεθ«»=+-]*)\]/\1/g'
+#
+#     # Otherwise sed removes only empty lines.
+#     else:
+#         return r'/^$/d;s/.*/\L\0/g' if options & BIT_INPUT_TO_LCASE else r'/^$/d'
 
 
 def get_sed_cmd_common_part(options: int) -> list:
@@ -29,16 +46,3 @@ def get_sed_cmd_common_part(options: int) -> list:
     :return:            List of sed arguments.
     """
     return ["sed", "-Ee", get_sed_regex(options)]
-
-
-# def get_sed_regex(options: int) -> str:
-#
-#     # If BIT_ULL_IN sed filters links leaving only sentences and removes square brackets around tokens if any.
-#     if options & BIT_ULL_IN:
-#         return r'/\(^[0-9].*$\)\|\(^$\)/d;s/\[\([a-z0-9A-Z.,:\@"?!*~()\/\#\$&;^%_`\0xe2\x27\xE2\x80\x94©®°•…≤±×΅⁻¹²³€αβπγδμεθ«»=+-]*\)\]/\1/g;s/.*/\L\0/g' \
-#             if options & BIT_INPUT_TO_LCASE \
-#             else r'/\(^[0-9].*$\)\|\(^$\)/d;s/\[\([a-z0-9A-Z.,:\@"?!*~()\/\#\$&;^%_`\0xe2\x27\xE2\x80\x94©®°•…≤±×΅⁻¹²³€αβπγδμεθ«»=+-]*\)\]/\1/g'
-#
-#     # Otherwise sed removes only empty lines.
-#     else:
-#         return r"/^$/d;s/.*/\L\0/g" if options & BIT_INPUT_TO_LCASE else r"/^$/d"

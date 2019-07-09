b9426975b7406e068844660a44a6770dc43f1742
andres <suarezandres@gmail.com>
2019-07-09 16:05:48 +0800

    rm_LEFTWALL.sh to handle either files or directories

--- src/common/rm_LEFTWALL.sh
+++ src/common/rm_LEFTWALL.sh
@@ -3,11 +3,30 @@
 # first field at the beginning of it.
 # Useful for cleaning ull files by DNN with initial LEFT-WALL
 
-# takes two arguments: file to get blocks from, file to blocks with no WALL
+# takes one argument: file or directory with files to remove LW
+# outputs processed files in directory with same name, appened by "-OLW"
 
-# usage: rm_LEFTWALL.sh file outfile
+# usage: rm_LEFTWALL.sh <file or dir>
 
-uniq $1 | awk -v RS='\n\n' 'sub("^" $1 FS, _) {print $0 "\n"}' > $2
+newdir=$1"-0LW";
+echo $newdir
+mkdir -p $newdir;
+
+if [ -d "$1" ]
+then
+	for file in $1/*
+	do
+	    filename=$(basename -- "$file");
+	    echo $filename;
+		uniq $filename | awk -v RS='\n\n' 'sub("^" $1 FS, _) {print $0 "\n"}' > $newdir/$filename;
+	done
+elif [ -f "$1" ]
+then
+	uniq $1 | awk -v RS='\n\n' 'sub("^" $1 FS, _) {print $0 "\n"}' > $newdir/$1;
+else echo "$1 is not valid";
+	exit 1;
+fi
+
 # uniq removes all duplicated newlines
 # awk 	-v RS defines the record separator as an empty line
 #		then awk removes first field at the beginning of the block
